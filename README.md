c4f0c96db577c1a2184c5c9a487b977cad8da830
haruki <nharuki@pm.me>
2025-06-29 09:30:57 +0900

    init project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Shizuku
+
+Modern microservices framework based on [Redis](https://redis.io/) and [RabbitMQ](https://www.rabbitmq.com/)
+
---
+++ shizuku-amqp/src/lib.rs
@@ -0,0 +1,16 @@
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::expect_used)]
+#![deny(clippy::panic)]
+#![warn(missing_docs)]
+#![deny(rustdoc::broken_intra_doc_links)]
+#![doc = include_str!("../../README.md")]
+
+/// internal errors
+pub mod error;
+
+/// tools for rabbitmq
+pub mod rabbitmq;
+
+/// tools for redis
+pub mod redis;
+
